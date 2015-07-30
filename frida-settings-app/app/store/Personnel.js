8bf2f2592afe08616aa7bd69fb7e407421700e72
Oleg Taranenko <olegtaranenko@gmail.com>
2015-07-30 04:15:17 +0200

    load personnel from mock rest

--- frida-settings-app/app/store/Personnel.js
+++ frida-settings-app/app/store/Personnel.js
@@ -1,5 +1,8 @@
 Ext.define('FridaSettings.store.Personnel', {
     extend: 'Ext.data.Store',
+    requires: [
+        'Ext.data.proxy.Rest'
+    ],
 
     alias: 'store.personnel',
 
@@ -7,18 +10,26 @@ Ext.define('FridaSettings.store.Personnel', {
         'name', 'email', 'phone'
     ],
 
-    data: { items: [
-        { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
-        { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
-        { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
-        { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
-    ]},
+    config: {
+        autoLoad: true
+    },
+
+    /*
+        data: { items: [
+            { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
+            { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
+            { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
+            { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
+        ]},
+    */
 
     proxy: {
-        type: 'memory',
+        type: 'rest',
+//        url: 'api',
+        url: 'api/user/list.json',
         reader: {
             type: 'json',
-            rootProperty: 'items'
+            rootProperty: 'results'
         }
     }
 });
